ba01574e96d247ec0d96b204c7bbbe45e088e0e0
Dhruv Patel <128783279+Dhruv0160@users.noreply.github.com>
2026-04-20 23:09:52 +0530

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,18 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-export default function ProtectedRoute({ children }) {
-    const { currentUser } = useAuth();
-    if (!currentUser) return <Navigate to="/login" replace />;
-    return children;
+
+export default function ProtectedRoute({ children, reqRole }) {
+  const { currentUser } = useAuth();
+
+  if (!currentUser) {
+    return <Navigate to="/login" replace />;
+  }
+
+  // If a specific role is required, and the user doesn't have it
+  // But Admin can access everything
+  if (reqRole && currentUser.role !== "admin" && currentUser.role !== reqRole) {
+    return <Navigate to="/" replace />; // Kick back to dash
+  }
+
+  return children;
 }
