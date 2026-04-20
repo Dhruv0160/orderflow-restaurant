6ad6b49bd597162a9dad6b0246162f3a0f22670b
Dhruv Patel <128783279+Dhruv0160@users.noreply.github.com>
2026-04-20 22:30:45 +0530

    Create utils/menuItems.js

---
+++ src/utils/menuItems.js
@@ -0,0 +1,4 @@
+export const MENU_ITEMS = [
+  { name: "Burger", price: 120, category: "Fast Food", image: "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=400" },
+  { name: "Pizza", price: 250, category: "Fast Food", image: "https://images.unsplash.com/photo-1513104890138-7c749659a591?w=400" }
+  ];
